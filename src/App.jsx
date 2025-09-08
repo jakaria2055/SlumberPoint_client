cca556adfd1b91091c28169192c4acfd5169bd62
Jakaria Ahmed <jakaria88520@gmail.com>
2025-09-08 23:10:15 +0600

    hotel registration & hotel owner

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,24 @@ import Footer from './components/Footer'
 import Rooms from './pages/Rooms'
 import RoomDetails from './pages/RoomDetails'
 import Booked from './pages/Booked'
+import HotelRegister from './components/HotelRegister'
+import Layout from './pages/HotelOwner/Layout'
 
 function App() {
   const isOwnerPath = useLocation().pathname.includes("owner")
   return (
     <>
      {!isOwnerPath && <NavBar />}
+     {false && <HotelRegister />}
      <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/rooms' element={<Rooms />} />
           <Route path='/rooms/:id' element={<RoomDetails />} />
           <Route path='/my-booking' element={<Booked />} />
-
+          <Route path='/owner' element={<Layout />}>
+            
+          </Route>
         </Routes>
      </div>
      <Footer />
